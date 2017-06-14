badb89abc4ecd440e22825dbcc68a31f9208954b
jpklein <jpklein@gmail.com>
2017-06-14 15:33:45 -0700

    Tests get request to movies endpoint

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -74,4 +74,34 @@ class AppTest extends \PHPUnit\Framework\TestCase
         // $actual = $response->getContent();
         // $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * @test
+     */
+    public function testGetRequestToMoviesEndpointReturnsData()
+    {
+        // Sets the Content-Type header
+        $this->client->setClient(new \GuzzleHttp\Client([
+            'headers' => ['Content-Type' => 'application/vnd.api+json']
+        ]));
+
+        // Sends the request
+        $this->client->request('GET', 'http://localhost:8080/movies/1');
+        $response = $this->client->getResponse();
+
+        // Compares HTTP status code
+        $expected = 200;
+        $actual = $response->getStatus();
+        $this->assertEquals($expected, $actual);
+
+        // Compares Content-Type header
+        $expected = 'application/vnd.api+json';
+        $actual = $response->getHeaders()['Content-Type'][0];
+        $this->assertEquals($expected, $actual);
+
+        // Compares page contents
+        $expected = '{"data":[{"type":"movies","id":"1","attributes":{"name":"Avatar"}}]}';
+        $actual = $response->getContent();
+        $this->assertEquals($expected, $actual);
+    }
 }
